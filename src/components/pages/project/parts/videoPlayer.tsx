2b47cc1462b54c1b42e8f5c3c531103d397c6ff6
pythongor <djmozartd@gmail.com>
2022-12-18 22:39:28 +0200

    lfs 9

--- src/components/pages/project/parts/videoPlayer.tsx
+++ src/components/pages/project/parts/videoPlayer.tsx
@@ -1,3 +1,30 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d18e97fd100769c8d23281f5c1a2616287a7b1c9264337acd06af44a04173a09
-size 699
+import React from "react";
+import { connect } from "react-redux";
+import { StateType } from "ducks/types";
+import ReactPlayer from "react-player/file";
+import styles from "../project.module.scss";
+
+type StateProps = ReturnType<typeof MSTP>;
+type DispatchProps = typeof MDTP;
+
+const VideoPlayer: React.FC<StateProps & DispatchProps> = ({
+  currentProject,
+}) => {
+
+  return (
+    <ReactPlayer
+      url={[{ src: currentProject?.video || "" }]}
+      controls={true}
+      width="100%"
+      height="100%"
+      className={styles.videoContainer}
+    />
+  )
+};
+
+const MSTP = ({ currentProject }: StateType) => ({
+  currentProject,
+});
+const MDTP = {};
+
+export default connect(MSTP, MDTP)(VideoPlayer);
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,36 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b9e9159f729098f7622783303d96b21245b41c374196dda57a36ce47d399acba
-size 1020
+export const mode = "development";
+export const devtool = "inline-source-map";
+export const entry = ["./src/app.tsx" /*main*/];
+export const output = {
+  filename: "./bundle.js", // in /dist
+};
+export const resolve = {
+  // Add `.ts` and `.tsx` as a resolvable extension.
+  extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
+};
+export const module = {
+  rules: [
+    { test: /\.tsx?$/, loader: "ts-loader" },
+    {
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "svg-url-loader",
+          options: {
+            limit: 10000,
+          },
+        },
+      ],
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        { loader: "style-loader" },
+        { loader: "css-modules-typescript-loader" },
+        { loader: "css-loader", options: { modules: true } },
+        { loader: "sass-loader" }, // to convert SASS to CSS
+        // NOTE: The first build after adding/removing/renaming CSS classes fails, since the newly generated .d.ts typescript module is picked up only later
+      ],
+    },
+  ],
+};
